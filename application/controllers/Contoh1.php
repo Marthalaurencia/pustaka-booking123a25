56b47bfadb017f5f760bf332707c2e0ca63a65d8
Marthalaurencia <marthamuslimah6@gmail.com>
2023-09-26 20:47:09 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller
+{   
+    public function index(){
+        echo "<h1> Perkenalan </h1>";
+        echo "Nama Saya Jeno
+        Saya tinggal di planet Mars
+        Olahraga yang saya sukai adalah panen koin shopee";
+    }
+}
+
+?>
